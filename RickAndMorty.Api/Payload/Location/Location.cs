0f944968d00948c2c638f6721e1763ef362a8bcf
Alexander <vsany@meta.ua>
2023-01-16 09:23:32 +0200

    [-] removed Payloads: Location and Origin

--- RickAndMorty.Api/Payload/Location/Location.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace RickAndMorty.Api.Payload.Location;
-
-public class Location
-{
-    [Required]
-    public string Dimension { get; set; }
-}
